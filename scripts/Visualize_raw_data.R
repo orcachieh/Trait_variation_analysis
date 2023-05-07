6159c69ea8c4ba7e295118dcaf6be15072dbe87b
orcachieh <chiehlin.tony@gmail.com>
2023-05-08 09:41:53 +1000

    Reorganize stucture a bit. Prepare for producing reable porduct.

--- scripts/Visualize_raw_data.R
+++ scripts/Visualize_raw_data.R
@@ -1,7 +1,9 @@
+#+ Read library, include = FALSE
 library(tidyverse)
 library(ggplot2)
 
-trait_dat <- read.csv("Raw_data.csv", header = TRUE)
+#+ Read data and Pivot to plotting format
+trait_dat <- read.csv("../Raw_data.csv", header = TRUE)
 
 trait_dat %>%
   mutate(Belowground_dry_mass1 = as.numeric(Belowground_dry_mass1)) %>%
@@ -11,8 +13,8 @@ trait_dat %>%
           !Belowground_dry_mass3 &
           !Flower_seed) %>%
   pivot_longer(cols = Canopyheight_1:Internodelength_10,
-               names_to = c(".value", "trails"),
+               names_to = c(".value", "trials"),
                names_sep = "_" ) -> trait_long_dat
 
 ggplot(data = trait_long_dat[trait_long_dat$abb=="HUN", ]) +
-  geom_density(aes(x=Leaflength))
+  geom_density(aes(x=Leafwidth))
