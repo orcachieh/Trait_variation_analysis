d44ce41591d9ef4fdf68b657c9bb8bed9313c6e6
Chieh Lin <chiehlin.tony@gmail.com>
2023-11-09 14:46:21 +1000

    Sort out the process of calculating fetch through sample point and coastal elevation layer. Towsville pcaluation is presented. The other location need to be finished as well

--- scripts/Environmental_covariates.R
+++ scripts/Environmental_covariates.R
@@ -18,41 +18,91 @@ library(waver)
 library(mapdata)
 library(maps)
 library(terra)
+library(ggplot2)
 
 #' This document present the 
 
 #' # All sample localities
-
+# read and cleanup sample point csv
 loc <- read.csv("../../Sample points/Sample_points_include_Tropwater.csv")
 
 loc %>%
-  select(lon, lat, name) -> loc
+  select(lon, lat, name, ele) -> loc
+
+# create a sf object and assign correct coordinate reference system (CRS) -> WGS84 (it's a records from GPS. Generally it should be WGS84
+loc_point <- st_as_sf(x = loc,
+                     coords = c("lon", "lat"),
+                     crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
 
 #' # Environmental covariates
 
 #' ## Relative physical exposure (fetch)
+#' There are three availalbe methods to reflect Relative physical exposure 
+#' 1. Effective fetch (EF), which present fetch from several angles and one fetch is an averagre of nearby fetch
+#' 2. Relative exposure Index (REI) multiply the EF by wind speed (Masom et. al, 2018).
+#' 3. Wave energy (package "waver") is calculated by three component: fetch, dominant wind speed, and depth.
 
-# read coastline data
+#' Starting with fetch. 
+#' In order to accomplish the problem of some intertidal point are higher than sea level and is "on land" if using normal coastal line, I use the intertidal elevation layer (Intertidal Extents Model 25 m v 2.0.0) from Geoscience Australia and specify shore line as level 7 instead of level 0.This mean the shore line is actually 7 meter elevation (model value) to calculate fetch.
+#' Using the consistent method for all sample points should be alright for calculate relative exposure among the sites.
 
+#' Due  to large geographic range. The shore line was separate to following location for easier fetch calculation - 
+#' to: Pallarenda + Maggi + Cleveland 
+#' cl: Clairview
+#' gl: Gladstone
+#' he: Hervey bay
+#' ka: karumba
+#' we: Weipa
+
+# read intertidal elevation layer (Intertidal Extents Model 25 m v 2.0.0)
 t1 <- rast("../Coastal_data/ITEM_REL_24_147.05_-18.95.tif")
 t2 <- rast("../Coastal_data/ITEM_REL_195_146.51_-18.95.tif")
 
-as.contour(t2) -> t2c
+# merge several raster to obtain proper range if required
+merge(t1, t2) -> to
 
-st_as_sf(t2c) -> t2sf
-# get world coastline polygon
+# create contour line from raster file
+as.contour(to) -> toc
 
-maps::map("worldHires", plot = FALSE, fill = TRUE) %>%
-  st_as_sf(crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0") -> whr
-sf_aus <- ozmap("states")
-st_crs(sf_aus) <- st_crs(sf_point)
+# transfer terra object to sf to compatible with "waver" package
+st_as_sf(toc)-> tosf
 
-sf_point <- st_as_sf(x = loc,
-                     coords = c("lon", "lat"),
-                     crs = st_crs(t1))
+# remove sediment sample
+# project sample points as coastline data, GDA. This process is required for fecth calculation by "waver" package
+loc_point_gda <- st_transform(loc_point, crs(tosf))
 
-fetch_len(sf_point[31, ], bearing = c(seq(0, 360, 30)), shoreline = t2sf, dmax = 40000)
+# calculate fetch for sample points. 12 fetch lines (360 degree each with 30 degree separation) are calculated for each point.
+# maximum fetch is 20000 m if no coastline present at that angle
 
+fetch_len_multi(loc_point_gda[1:47, ], bearing = c(seq(0, 330, 30)), shoreline =tosf[tosf$level == 7,], dmax = 20000) -> to_fetch
+
+# Rearrange fetch matrix for geom_spoke plotting
+# There is a bit of work due to angles are seen differently from two function.
+# The fetch were calculated from north and clockwise, but geom_spoke plot from east (0 degree of x axis) and counter clockwise.
+
+to_fetch %>%
+  as_tibble() %>%
+  pivot_longer(everything(), names_to = "angle", values_to = "fetch") %>%
+  mutate(angle = (abs(as.numeric(angle)-360)+90)*pi/180,
+         # Re-assign angle and transform to radian
+         X = rep(st_coordinates(loc_point_gda[1:47, ])[, 1], each = 12),
+         Y = rep(st_coordinates(loc_point_gda[1:47, ])[, 2], each = 12)) -> to_spoke
+# plotting fetch and coastline
+
+ggplot() +
+  geom_sf(data = tosf[tosf$level == 7,]) +
+  geom_spoke(data = to_spoke, 
+             aes(x = X, y = Y, angle = angle, radius = fetch)) +
+  geom_sf(data = loc_point_gda[1:47, ], color = "red") +
+  coord_sf(xlim = c(min(st_coordinates(loc_point_gda[1:47, ])[, 1]) - 20000,
+                    max(st_coordinates(loc_point_gda[1:47, ])[, 1]) + 20000),
+           ylim = c(min(st_coordinates(loc_point_gda[1:47, ])[, 2]) - 20000,
+                    max(st_coordinates(loc_point_gda[1:47, ])[, 2]) + 20000))
+
+
+# Bathymetry data obtain from NOAA (highest resolution 0.5 degree)
+# Problem intertidal points are actually above higher than sea level
 
 qldt <- getNOAA.bathy(lon1 = 146.5, lon2 = 148, lat1 = -19, lat2 = -19.5, resolution = 0.5)
 
